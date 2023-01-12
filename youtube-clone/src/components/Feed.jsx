af07d11d5c207342ddfac436fea1c8f558dbb8e2
Pard3sh7 <P3sh22@gmail.com>
2023-01-11 22:27:29 -0500

    time stamp 3

--- youtube-clone/src/components/Feed.jsx
+++ youtube-clone/src/components/Feed.jsx
@@ -1,8 +1,49 @@
-import React from 'react'
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+import { Sidebar, Videos} from './';
 
+
+// feed will consist of side bar and then video feed on the right
 const Feed = () => {
   return (
-    <div>Feed</div>
+    <Stack
+      sx={{ flexDirection: { sx: 'column', md: 'row'}}}
+    >
+      {/* it will be a column by default, but if it is a medium sized device or larger than it will be row view*/}
+      <Box
+        sx={{ height: { sx: 'auto', md: '92vh' }, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2}}}
+      >
+        <Sidebar/>
+        <Typography className='copyright' variant='body2' sx={{mt: 1.5, color: '#fff'}}>
+          Copyright 2022 JDTube
+        </Typography>
+      </Box>
+
+      <Box 
+        p={2}
+        sx={{ overflowY: 'auto', height: '90vh', flex: 2}}
+      >
+        <Typography 
+          variant='h4'
+          fontWeight='bold'
+          mb={2}
+          sx={{
+            color: 'white'
+          }}
+        >
+          Category <span 
+            style={{ color: '#F31503' }}
+          >
+            videos
+          </span>
+        </Typography>
+        <Videos
+          videos={[]}
+        />
+      </Box>
+
+    </Stack>
   )
 }
 
